6d7393871aa4229558e2482241a99b45ed33ff13
Thomas Megantz <thomas@peernova.com>
2019-05-13 11:49:26 -0700

    Removed copyright

--- src/main/java/mariojumpsim/KeyListener.java
+++ src/main/java/mariojumpsim/KeyListener.java
@@ -1,16 +1,3 @@
-// Copyright (C) 2019 PeerNova, Inc.
-//
-// All rights reserved.
-//
-// PeerNova and Cuneiform are trademarks of PeerNova, Inc. References to
-// third-party marks or brands are the property of their respective owners.
-// No rights or licenses are granted, express or implied, unless set forth in
-// a written agreement signed by PeerNova, Inc. You may not distribute,
-// disseminate, copy, record, modify, enhance, supplement, create derivative
-// works from, adapt, or translate any content contained herein except as
-// otherwise expressly permitted pursuant to a written agreement signed by
-// PeerNova, Inc.
-
 package mariojumpsim;
 
 import java.awt.event.KeyAdapter;
